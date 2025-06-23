60a45ca86831dd16ac21a07a73500c1aefdc4c55
ParkMinsoo <harimaho88@gmail.com>
2025-06-23 14:44:27 +0900

    feat : DeckManager

---
+++ Assets/Script/PMS/DeckManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckManager
+{
+    //public DeckData currentDeck = new();
+
+    //public bool TryAddUnitToDeck()
+    //{
+
+    //}
+  
+
+    
+    public bool AddToDeck() // 덱 리스트에 추가
+    {
+        return true;
+    }
+
+    public bool RemoveFromDeck() // 덱 리스트에서 제거
+    {
+        return true;
+    }
+
+
+    
+
+}
