c3e50fc0c0f0e5e4841287a45377e1490b6436e1
피타쵸쵸 <djfb123@naver.com>
2025-06-23 16:49:56 +0900

    [add] Function

---
+++ Assets/Script/EDH/GottchaUI.cs
@@ -0,0 +1,39 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using Button = UnityEngine.UI.Button;
+
+public class GottchaUI : MonoBehaviour
+{
+    [SerializeField]
+
+    public Button SpritPickBtn;
+    public Button UndeadPickBtn;
+    public Button NormalPickBtn;
+
+    public void Start()
+    {
+        SpritPickBtn.onClick.AddListener(Sprit);
+        UndeadPickBtn.onClick.AddListener(Undead);
+        NormalPickBtn.onClick.AddListener(Normal);
+    }
+
+    public void Sprit()
+    {
+        Button.ButtonClickedEvent buttonClicked = new Button.ButtonClickedEvent();
+        
+
+
+
+        SpritPickBtn.onClick.RemoveAllListeners();
+    }
+
+    public void Undead()
+    {
+        UndeadPickBtn.onClick.RemoveAllListeners();
+    }
+    public void Normal()
+    {
+        NormalPickBtn.onClick.RemoveAllListeners();
+    }
+}
---
+++ Assets/Script/EDH/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    public void CloseTab()
+    {
+        SceneManager.LoadScene("MainScene");    
+    }
+}
