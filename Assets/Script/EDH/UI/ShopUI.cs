f6e15f37977c208ad03a7610adac2208775b6415
DHo <djfb123@naver.com>
2025-06-26 20:25:33 +0900

    [fix] ShopUI CS added
    [fix] ShopUI CS added

--- Assets/Script/EDH/UI/ShopUI.cs
+++ Assets/Script/EDH/UI/ShopUI.cs
@@ -17,7 +17,7 @@ public class ShopUI : UIBase
         //프리펩으로 만들어서  직접 찾아와야함.  무조건 코드로 가져와야함.
         uiManager = FindObjectOfType<UIManager>();
         ExitBtn.onClick.AddListener(() => UIManager.Instance.Close<ShopUI>());
-        // 나머지 창은 비활성화 메인제외,  가독성 측면에서 않좋을때 나누기.
+        // 나머지 창은 비활성화 메인제외,  가독성 측면에서 안좋을때 나누기.
         ExitBtn.onClick.AddListener(() => UIManager.Instance.Close<HQUI>());
         ExitBtn.onClick.AddListener(() => UIManager.Instance.Close<StageUI>());
         ExitBtn.onClick.AddListener(() => UIManager.Instance.Close<GottaUI>());
