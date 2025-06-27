ca83847fbb5ca72cd608a99eae7b3f8c222979c0
meatballm <wh9555115@naver.com>
2025-06-27 10:25:39 +0900

    add : HQUI 구현중

---
+++ Assets/Script/JSM/HQ/BuildingSpawner.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BuildingSpawner : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+    public Transform parentObject;
+    public int count = 5;
+    public ScrollRect scrollRect;
+
+    private void Start()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            GameObject newObj = Instantiate(prefabToSpawn, parentObject);
+            newObj.name = $"{prefabToSpawn.name}_{i}";
+
+            Button button = newObj.GetComponentInChildren<Button>();
+            BuildSlotUI buildSlotUI = newObj.GetComponentInChildren<BuildSlotUI>();
+            if (button != null)
+            {
+                button.onClick.AddListener(() =>
+                {
+                    buildSlotUI.Select();
+                });
+            }
+        }
+    }
+}
