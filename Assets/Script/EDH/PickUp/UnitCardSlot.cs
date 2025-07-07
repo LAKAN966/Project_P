64f0c31f238ea44034390e26a1235771b597f4f4
DHo <djfb123@naver.com>
2025-07-07 21:26:36 +0900

    [fix] Data Passing (1)

--- Assets/Script/EDH/PickUp/UnitCardSlot.cs
+++ Assets/Script/EDH/PickUp/UnitCardSlot.cs
@@ -5,21 +5,22 @@ using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEditor.Progress;
 using UnityEngine.UI;
+using System;
 
 public class UnitCardSlot : MonoBehaviour
 {
-    [SerializeField] private TMP_Text UnitICardNametext;        //유닛 이름
+    [SerializeField] private TMP_Text UnitICardNametext;        // 유닛 이름
 
     [SerializeField] public Image UnitIcon;                     // 유닛 아이콘
-    
+    Dictionary<int, PickInfo> PickInfo = PickUpListLoader.Instance.GetAllPickList();
     public void init(PickInfo pickInfo)
     {
         GameObject root = transform.root.gameObject;
         Debug.Log(root.name);
 
-        pickInfo = (pickInfo != null) ? pickInfo : new PickInfo();
 
-        UnitICardNametext.text = pickInfo.Name;      // 이름
-        // UnitIcon.sprite = pickInfo.image     // 이미지
-       }
+        pickInfo.Name = UnitICardNametext.text;    // 이름
+
+        pickInfo.Uniticon = UnitIcon.sprite;       // 이미지
+    }
 }
