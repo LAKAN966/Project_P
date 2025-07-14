9040845886491875b1c879b701ee025d90cae8da
DHo <djfb123@naver.com>
2025-07-14 17:10:58 +0900

    [add] : Frame Set, SoundManager added

--- Assets/Script/EDH/Settings/FrameSetting.cs
+++ Assets/Script/EDH/Settings/FrameSetting.cs
@@ -7,6 +7,9 @@ public class FrameSetting : MonoBehaviour
 {
     public Button LowFPSbtn; // 30프레임
     public Button HighFPSbtn;// 60프레임
+    
+    public Image spriteRendererOne; // 색상1
+    public Image SpriteRendererTwo; // 색상2
     // Start is called before the first frame update
     void Start()
     {
@@ -14,18 +17,22 @@ public class FrameSetting : MonoBehaviour
         HighFPSbtn.onClick.AddListener(SetHighFPS);
     }
 
-
     public void SetLowFPS()
     {
+        spriteRendererOne.color = Color.green;
+        Debug.Log("나는 초록");
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 30;
         Debug.Log(Application.targetFrameRate);
+        SpriteRendererTwo.color = Color.white;
     }
 
     public void SetHighFPS()
     {
+        SpriteRendererTwo.color = Color.green; 
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
         Debug.Log(Application.targetFrameRate);
+        spriteRendererOne.color = Color.white;
     }
 }
