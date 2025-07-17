91dabc10d51b97078e8a1b43cfc341c150dcc475
DHo <djfb123@naver.com>
2025-07-17 11:35:27 +0900

    [add}  소리 테스트

--- Assets/Script/EDH/Settings/SoundManager.cs
+++ Assets/Script/EDH/Settings/SoundManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : Singleton<SoundManager>
+public class SoundManager :MonoBehaviour
 {
     AudioSource audioSource;
     public AudioClip clip;
