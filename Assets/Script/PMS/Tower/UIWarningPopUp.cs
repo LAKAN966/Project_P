9ba2bcca30da90eea777b818ac67ae96241ee0b2
ParkMinsoo <harimaho88@gmail.com>
2025-07-18 17:56:11 +0900

    fix : 경고 팝업 코루틴 수정

---
+++ Assets/Script/PMS/Tower/UIWarningPopUp.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWarningPopUp : MonoBehaviour
+{
+    private Coroutine hideCoroutine;
+
+    private void OnEnable()
+    {
+        if (hideCoroutine != null)
+            StopCoroutine(hideCoroutine);
+
+        hideCoroutine = StartCoroutine(HideRoutine());
+    }
+
+    private IEnumerator HideRoutine()
+    {
+        yield return new WaitForSeconds(1f);  
+        gameObject.SetActive(false);
+        hideCoroutine = null;
+    }
+
+}
