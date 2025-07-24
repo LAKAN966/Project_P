1bef6c1110f1c45920be959f1e0e4d7acbd14111
DHo <djfb123@naver.com>
2025-07-24 14:17:12 +0900

    fix : 소리 매니저 중간 작업 저장

--- Assets/Script/EDH/Settings/SoundManager.cs
+++ Assets/Script/EDH/Settings/SoundManager.cs
@@ -6,9 +6,18 @@ public class SoundManager :MonoBehaviour
 {
     public AudioSource bgmSource1; // 첫 번째 음악 소스
     public AudioSource bgmSource2; // 두 번째 음악 소스
+    public AudioSource bgmSource3; // 두 번째 음악 소스
 
-    public AudioClip clip1; // 첫 번째 음악
-    public AudioClip clip2; // 두 번째 음악
+    public AudioClip clip1; // 메인 로비 BGM
+    public AudioClip clip2; // SFX
+    public AudioClip clip3; // 모집
+    public AudioClip clip4; // 유닛관리
+    public AudioClip clip5; // 전초기지
+    public AudioClip clip6; // 상점
+    public AudioClip clip7; // 일반 스테이지 
+    public AudioClip clip8; // 보스 스테이지
+    public AudioClip clip9; // 골드던전 전투
+    public AudioClip clip10;// 미궁의탑 전투
 
     void Start()
     {
