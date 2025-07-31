2dc2e651f26dd6438c2eec7082e6389b480025da
meatballm <wh9555115@naver.com>
2025-07-31 20:25:03 +0900

    fix : 튜토리얼 수정중

--- Assets/Script/JSM/Battle/Manager/BattleManager.cs
+++ Assets/Script/JSM/Battle/Manager/BattleManager.cs
@@ -26,7 +26,7 @@ public class BattleManager : MonoBehaviour {
 
     public void StartBattle(int selectedStageID, List<UnitStats> normalDeck, UnitStats leaderDeck, int stageType = 0)
     {
-        Debug.Log(selectedStageID + "스타트 배틀");
+        //Debug.Log(selectedStageID + "스타트 배틀");
         WaveManager.Instance.stageID = selectedStageID;
         WaveManager.Instance.stageType = stageType;
         UnitSpawner.Instance.Init(normalDeck, leaderDeck);
